6bca8ad24580ca2fb002ab6bbdaabd87dab1d00a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-10-17 22:34:23 -0300

    Implementando Paciente Service

---
+++ api.agenda.de.compromissos/Interfaces/Services/IPacienteService.cs
@@ -0,0 +1,13 @@
+﻿using api.agenda.de.compromissos.Models;
+using System.Collections.Generic;
+
+namespace api.agenda.de.compromissos.Interfaces.Services
+{
+    public interface IPacienteService
+    {
+        void Incluir(PacienteModel paciente);
+        void Alterar(PacienteModel paciente);
+        void Excluir(int id);
+        IList<PacienteModel> Buscar();
+    }
+}
---
+++ api.agenda.de.compromissos/Services/PacienteService.cs
@@ -0,0 +1,37 @@
+﻿using api.agenda.de.compromissos.Interfaces.Services;
+using api.agenda.de.compromissos.Models;
+using System.Collections.Generic;
+
+namespace api.agenda.de.compromissos.Services
+{
+    public class PacienteService : IPacienteService
+    {
+
+        IPacienteService _pacienteService;
+
+        public PacienteService(IPacienteService pacienteService)
+        {
+            _pacienteService = pacienteService;
+        }
+
+        public void Alterar(PacienteModel paciente)
+        {
+            _pacienteService.Alterar(paciente);
+        }
+
+        public IList<PacienteModel> Buscar()
+        {
+            return _pacienteService.Buscar();
+        }
+
+        public void Excluir(int id)
+        {
+            _pacienteService.Excluir(id);
+        }
+
+        public void Incluir(PacienteModel paciente)
+        {
+            _pacienteService.Incluir(paciente);
+        }
+    }
+}
